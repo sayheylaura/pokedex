8d15bcf6545d41b5544c66926ffaca51e75116b3
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-10 17:38:51 +0100

    Create docs folder

--- docs/precache-manifest.271b773cfae22ddea4e698b7e76332ff.js
+++ docs/precache-manifest.271b773cfae22ddea4e698b7e76332ff.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.9eb600ee.js"
   },
   {
-    "revision": "6c69b50dbd18fce5a3a0",
-    "url": "./static/js/main.6c69b50d.chunk.js"
+    "revision": "3b74d114d5fd32bbcf11",
+    "url": "./static/js/main.3b74d114.chunk.js"
   },
   {
     "revision": "eefc689aec6ea395f42e",
     "url": "./static/js/2.eefc689a.chunk.js"
   },
   {
-    "revision": "6c69b50dbd18fce5a3a0",
-    "url": "./static/css/main.a6c9f069.chunk.css"
+    "revision": "3b74d114d5fd32bbcf11",
+    "url": "./static/css/main.95996403.chunk.css"
   },
   {
-    "revision": "d8cad358b387fdc2d3aa89c6277a1c32",
+    "revision": "902c7b90c7ff9e62a94ed004861103ab",
     "url": "./index.html"
   }
 ];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7191f57a547172aa34ff2a35b32f768e.js"
+  "./precache-manifest.271b773cfae22ddea4e698b7e76332ff.js"
 );
 
 workbox.clientsClaim();
